c9bb321a423f0eda87eecaecb6fe1a4c9df11b06
nemethrobert59 <123586949+nemethrobert59@users.noreply.github.com>
2024-01-10 21:28:45 +0100

    test sql file has been added

---
+++ market_data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE market_data (id SERIAL PRIMARY KEY,
+datetime TIMESTAMP NOT NULL,
+open NUMERIC(10, 2),
+high NUMERIC(10, 2),
+low NUMERIC(10, 2),
+close NUMERIC(10, 2),
+volume BIGINT
+);
