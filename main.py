a3a44fa818fd66b726d3df1fdf1473f71ba86b72
nemethrobert59 <123586949+nemethrobert59@users.noreply.github.com>
2024-01-19 18:38:29 +0100

    logging has been added

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import file_helper
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s:%(message)s', datefmt='%Y-%m-%d %H:%M:%S %Z', level=logging.DEBUG)
 
 if __name__ == '__main__':
     file_config = file_helper.read_config_file(config_type="metadata")
@@ -6,3 +9,4 @@ if __name__ == '__main__':
     print(file_config["file_location"])
     db_config = file_helper.read_config_file(config_type="database")
     print(db_config)
+    logging.info("We are here")
