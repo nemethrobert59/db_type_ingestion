7029b50c5ded29496c7eafcb6e0b4138111bc073
robert.nemeth <nemethrobert59@gmail.com>
2024-01-26 19:17:42 +0100

    init files has been added

--- ddl/market_data.sql
+++ ddl/market_data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE market_data (id SERIAL PRIMARY KEY,
+CREATE TABLE my_schema.market_data (id SERIAL PRIMARY KEY,
 datetime TIMESTAMP NOT NULL,
 open NUMERIC(10, 2),
 high NUMERIC(10, 2),
